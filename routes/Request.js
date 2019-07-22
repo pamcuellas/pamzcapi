2172547a522445ef7c0d8fdcdfef93c7151f930f
pamcuellas <pamcuellas@gmail.com>
2019-07-22 11:04:58 -0400

    Requests file modified.

--- routes/Request.js
+++ routes/Request.js
@@ -2,21 +2,6 @@ const express = require("express");
 const router = express.Router();
 const db 		= require("../models");
 
-// // List Requests
-// router.get("/", (req, res) => {
-// 	db.Request.find() // Here, remember that we are using Promise. That means this code could be different.
-// 	.then(  requests => res.json(requests) )
-// 	.catch( err  	 => res.send(err	 ) );
-// });
-
-// // Create a new Request
-// router.post("/", (req, res) => {
-// 	db.Request.create( req.body ) 
-// 	.then(  newRequest => res.status(201).json(newRequest) ) // 201 here is the code for successfull created.
-// 	.catch( err  	 => res.send(err	 ) );
-// });
-
-
 const helperRequest = require("../helpers/request");
 
 router.route('/')
